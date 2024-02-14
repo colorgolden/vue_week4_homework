0bdddf65f5e0ce99822427c247e5db1752474518
colorgolden <147834687+colorgolden@users.noreply.github.com>
2024-02-14 21:58:35 +0800

    update gh-pages 2

--- all.js
+++ all.js
@@ -72,7 +72,7 @@ const app = {
       const token = document.cookie.replace(/(?:(?:^|.*;\s*)newToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
       axios.defaults.headers.common['Authorization'] = token;
 
-      if (!window.location.href.includes('index.html')) {  //只要不是在admin_login頁面，就執行大括號內的程式碼
+      if (!window.location.href.includes('index.html')) {  //只要不是在index頁面，就執行大括號內的程式碼
         // #4  確認是否登入
         axios.post(`${url}/v2/api/user/check`, {}, { headers: { 'Authorization': token } })
           .then((res) => {
@@ -80,7 +80,7 @@ const app = {
           })
           .catch((error) => {
             alert("尚未登入會員，請重新登入！");
-            window.location.href = 'admin_login.html'; //跳轉到login頁面
+            window.location.href = 'index.html'; //跳轉到login頁面
             console.dir(error);
             return;
           })
